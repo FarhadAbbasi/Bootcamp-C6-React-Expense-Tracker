92fbe259e0db5906c3156eab26c8ff270791d2ba
Farhad Ali Abbasi <34246950+FarhadAbbasi@users.noreply.github.com>
2024-11-08 10:40:42 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+ 
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+// https://github.com/FarhadAbbasi/Bootcamp-C6-React-Expense-Tracker.git
+// c8e796cfa125005e7adf2142216d5bb5
